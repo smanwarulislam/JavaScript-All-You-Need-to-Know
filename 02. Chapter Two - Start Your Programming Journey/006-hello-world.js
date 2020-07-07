7b33b489da6503fb8cabfc16caf7a292ebc3ba85
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-08 05:01:42 +0600

    Add notes through the comments

--- 02. Chapter Two - Start Your Programming Journey/006-hello-world.js
+++ 02. Chapter Two - Start Your Programming Journey/006-hello-world.js
@@ -1,8 +1,33 @@
-console.log('Hello, World!');
-console.log('I love Bangladesh');
-console.log(45);
-console.log(60.789);
+// Q: What is Programming?
+// A: Programming is a set of instruction which our computer can read, understand, and produce and output.
 
-console.log('My favorite number: ' + 1);
+// Extensions
+// Your_Name.js
+
+// White Space Usually Ignored by the Compiler
+
+// console.log(Your Text)
+
+// String
+// Anything Wrapped with Quote is Called String
+
+console.log('Hello, World!');
+// expected output: "Hello, World!"
+
+console.log('I love Bangladesh');
+// expected output: "I love Bangladesh"
+
+console.log(45);
+// expected output: 45
+
+console.log(60.789);
+// expected output: 60.789
+
+console.log('My favorite number: ' + 1); // String + Number = String
+// expected output: "My favorite number: 1"
+
 console.log(1 + 1);
+// expected output: 2
+
 console.log('1' + 1);
+// expected output: "11"
