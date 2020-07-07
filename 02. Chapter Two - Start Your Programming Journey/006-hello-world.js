bdfaa0d975828ed595f44d9db3c5900c71d97b30
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-08 00:59:43 +0600

    hello world file has been added

---
+++ 02. Chapter Two - Start Your Programming Journey/006-hello-world.js
@@ -0,0 +1,8 @@
+console.log('Hello, World!');
+console.log('I love Bangladesh');
+console.log(45);
+console.log(60.789);
+
+console.log('My favorite number: ' + 1);
+console.log(1 + 1);
+console.log('1' + 1);
