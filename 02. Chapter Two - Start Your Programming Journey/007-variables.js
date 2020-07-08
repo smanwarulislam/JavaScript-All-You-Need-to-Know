502e671dfd5ab08db798756dede9ac11c6955a4a
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-08 23:30:39 +0600

    Add Variables

---
+++ 02. Chapter Two - Start Your Programming Journey/007-variables.js
@@ -0,0 +1,36 @@
+// Chapter Two
+// Understanding Variables
+
+// Variables
+
+// Variable is a Container which can Store Data
+
+// var name = 'S M Anwarul Islam';
+// var age = 30;
+
+// console.log(name);
+// expected output: S M Anwarul Islam
+
+// console.log(age);
+// expected output: 30
+
+console.log(name + ' knows JavaScript');
+// expected output: S M Anwarul Islam knows JavaScript
+
+console.log('His age is ' + age);
+// expected output: His age is 30
+
+console.log(name + ' is creating a JavaScript Course for All');
+// expected output: S M Anwarul Islam is creating a JavaScript Course for All
+
+console.log('But his age is ' + age);
+// expected output: But his age is 30
+
+var name = 'S M Anwarul Islam';
+var age = 30;
+
+// expected output:
+// undefined knows JavaScript
+// His age is undefined
+// undefined is creating a JavaScript Course for All
+// But his age is undefined
