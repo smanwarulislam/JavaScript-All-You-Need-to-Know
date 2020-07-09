9eb7b650c54b1dc5401978e58c3cda6f563af37b
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-09 22:15:19 +0600

    Add Reserved Words and Naming Convention

---
+++ 02. Chapter Two - Start Your Programming Journey/008-reserved-words-and-naming-convention.js
@@ -0,0 +1,16 @@
+// Chapter Two
+// Reserved Words & Naming Convention
+
+// Reserved words in JavaScript
+
+const else = 'something';
+// expected output: Uncaught SyntaxError: Unexpected token 'else'
+
+console.log(else);
+// expected output: Uncaught SyntaxError: Unexpected token 'else'
+
+// Naming Convention in JavaScript
+
+var mathNumber = 10;
+var accountNumberDetailsId = 104;
+var account_number_details_id = 105;
