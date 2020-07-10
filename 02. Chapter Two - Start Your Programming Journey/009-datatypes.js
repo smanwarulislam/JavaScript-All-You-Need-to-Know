54308087f7385010e2f9a15d1cb601f028a79d7a
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-10 21:50:31 +0600

    Add Data Types

---
+++ 02. Chapter Two - Start Your Programming Journey/009-datatypes.js
@@ -0,0 +1,26 @@
+// Chapter Two
+// Data Types in JavaScript
+
+// primitive
+// Primitive Types are Defined by JavaScript
+
+- Number
+10, 4.5, 8.12, 3.14
+
+- String
+'this is a text' + "This is also text"
+
+- Boolean
+true
+false
+
+- undefined
+
+- null
+
+// object
+// Object Types are User-Defined Data Type
+
+- Array
+- Object
+- Function
