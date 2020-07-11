66e7380d0167afbdc45a06236dd0134fee59645f
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-11 21:30:38 +0600

    Add Basic Strings

---
+++ 02. Chapter Two - Start Your Programming Journey/011-strings.js
@@ -0,0 +1,20 @@
+// Chapter Two
+// String in JavaScript
+
+// ********** String **********
+
+// String
+// Anything Wrapped Between Quotes is Called String
+
+// string literal
+var str = 'String';
+var str2 = "String";
+var str3 = `String`; // es6
+
+// string constructor
+var str4 = String('abcdefghij');
+var str5 = String(156);
+var str6 = String(3.1416);
+
+console.log(str4, str5, str6);
+// expected output: abcdefghij 156 3.1416
