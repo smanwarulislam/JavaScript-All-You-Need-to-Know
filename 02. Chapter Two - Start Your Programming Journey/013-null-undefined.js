b5b51545b315b27aa98c87d88115cc9a88c7664f
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-11 23:51:51 +0600

    Add Null & Undefined

---
+++ 02. Chapter Two - Start Your Programming Journey/013-null-undefined.js
@@ -0,0 +1,21 @@
+// Chapter Two
+// Null vs Undefined
+
+// ********** Null vs Undefined **********
+
+// undefined
+// A Variable without Value Always Store Undefined
+
+// null
+// A Variable with Unknown Value can Store Null
+
+var abc;
+var xyz = null;
+
+console.log(abc);
+// expected output: undefined
+
+console.log(xyz);
+// expected output: null
+
+// Null is a Special Type of Data Used as an Absence of an Object
