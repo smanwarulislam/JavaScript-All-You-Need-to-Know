0d0c55b17aa4319c3e944ab69e3dcb52031b86af
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-11 22:55:26 +0600

    Add Basic Booleans

---
+++ 02. Chapter Two - Start Your Programming Journey/012-booleans.js
@@ -0,0 +1,28 @@
+// Chapter Two
+// Boolean in JavaScript
+
+// ********** Booleans **********
+
+// Ask Yourself Anything!
+// If the answer always be either true or false then you can call it Boolean
+
+// Q: Should I Go Out?
+// A: Yes - True or
+// A: No - False
+
+// Q: Is It Raining?
+// A: No - False or
+// A: Yes - True
+
+// We have to think everything straight when it comes to solving the problem using a programming language.
+
+// boolean literal
+var b1 = true;
+var b2 = false;
+
+// boolean constructor
+var b3 = Boolean(true);
+var b4 = Boolean(false);
+
+console.log(b3, b4);
+// expected output: true false
