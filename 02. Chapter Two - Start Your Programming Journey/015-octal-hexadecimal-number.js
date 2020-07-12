61f41d20b61c65de97ab3782d2975babe78940f5
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-12 23:52:27 +0600

    Add Octal and Hexadecimal Number

---
+++ 02. Chapter Two - Start Your Programming Journey/015-octal-hexadecimal-number.js
@@ -0,0 +1,17 @@
+// Chapter Two
+// Octal & Hexadecimal Numbers
+
+/* Hexadecimal is a Base 16 Number System
+Where Octal is a Base 8 Number System */
+
+// Hexadecimal Number
+var hex = 0xff;
+
+console.log(hex);
+// expected output: 255
+
+// Octal Number
+var oct = 0756;
+
+console.log(oct);
+// expected output: 494
