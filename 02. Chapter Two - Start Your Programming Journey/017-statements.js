2a92b0166805b82d8b074a820c8cea245abc75c0
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-14 21:50:13 +0600

    Add Statements

---
+++ 02. Chapter Two - Start Your Programming Journey/017-statements.js
@@ -0,0 +1,14 @@
+// Chapter Two
+// Statements in JavaScript
+
+console.log('Hello, World!');
+// expected output: "Hello, World!"
+
+var str = 'string';
+
+var number = 10 + (30 / 5) * 5;
+
+var a = 10;
+var b = 20;
+
+var c = a >= b;
