0cf5e5f795de6663a07dc7ea8d550ecc8833d16b
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-15 20:10:55 +0600

    Add Math Functions

---
+++ 02. Chapter Two - Start Your Programming Journey/018-math.js
@@ -0,0 +1,78 @@
+// Chapter Two
+// JavaScript Math Functions
+
+// Functions are core building blocks of any programming language.
+// We will discuss about function in detail in future chapters.
+
+console.log(Math.E);
+// expected output: 2.718281828459045
+
+console.log(Math.PI);
+// expected output: 3.141592653589793
+
+var n = 4.589;
+
+console.log(Math.abs(n));
+// expected output: 4.589
+
+console.log(Math.floor(n));
+// expected output: 4
+
+console.log(Math.ceil(n));
+// expected output: 5
+
+console.log(Math.round(n));
+// expected output: 5
+
+var n = 4.389;
+
+console.log(Math.round(n));
+// expected output: 4
+
+console.log(Math.max(400, 500));
+// expected output: 500
+
+console.log(Math.min(400, 500));
+// expected output: 400
+
+console.log(Math.max(400, 500, 600));
+// expected output: 600
+
+console.log(Math.min(400, 500, 200));
+// expected output: 200
+
+console.log(Math.pow(2, 3));
+// expected output: 8
+
+console.log(Math.pow(3, 5));
+// expected output: 243
+
+console.log(Math.sqrt(9));
+// expected output: 3
+
+console.log(Math.sqrt(64));
+// expected output: 8
+
+console.log(Math.random());
+// expected output: 0.21493133091421157
+
+console.log(Math.random());
+// expected output: 0.8726674964359189
+
+console.log(Math.random());
+// expected output: 0.631299034111819
+
+console.log(Math.round(Math.random() * (50 + 1)));
+// expected output: 2
+
+console.log(Math.round(Math.random() * (50 + 1)));
+// expected output: 20
+
+console.log(Math.round(Math.random() * (50 + 1)));
+// expected output: 40
+
+console.log(Math.round(Math.random() * (50 + 1)));
+// expected output: 14
+
+console.log(Math.round(Math.random() * (50 + 1)));
+// expected output: 15
