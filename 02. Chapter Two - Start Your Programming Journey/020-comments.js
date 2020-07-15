2809675b56bb673764abd09ca8b61d528a641add
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-15 23:39:29 +0600

    Add Comments

---
+++ 02. Chapter Two - Start Your Programming Journey/020-comments.js
@@ -0,0 +1,51 @@
+// Chapter Two
+// Comments in JavaScript
+
+// Variables
+
+// Variable is a Container which can Store Data
+
+// var name = 'S M Anwarul Islam';
+// var age = 30;
+
+// console.log(name);
+// expected output: S M Anwarul Islam
+
+// console.log(age);
+// expected output: 30
+
+// these some lines will log some outputs
+
+console.log(name + ' knows JavaScript');
+// expected output: S M Anwarul Islam knows JavaScript
+
+console.log('His age is ' + age);
+// expected output: His age is 30
+
+console.log(name + ' is creating a JavaScript Course for All');
+// expected output: S M Anwarul Islam is creating a JavaScript Course for All
+
+console.log('But his age is ' + age);
+// expected output: But his age is 30
+
+var name = 'S M Anwarul Islam';
+var age = 30;
+
+// expected output:
+// undefined knows JavaScript
+// His age is undefined
+// undefined is creating a JavaScript Course for All
+// But his age is undefined
+
+// Single Line Comments
+
+// This is a one line JavaScript comment
+// this is a comment
+// this is another comment
+// this is the third line of comment
+
+// Multi-line Comments
+
+/* this is a comment
+this is another comment
+this is the third line of comment */
