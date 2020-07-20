344c18362526bbd55de108a0c0c1932140bdf594
smanwarulislam <engg.anwarbd@gmail.com>
2020-07-20 23:39:53 +0600

    Add Loops in Programming

---
+++ 04. Chapter Four - Reduce Repeating Tasks with Loops/029-loops-and-iteration.js
@@ -0,0 +1,19 @@
+// Chapter Four
+// Loops and iteration
+
+// Loops
+// Loops are Statements Which Execute Some Block of Code Repeatedly until The Condition Becomes False
+
+console.log('YourName');
+console.log('YourName');
+console.log('YourName');
+console.log('YourName');
+...
+console.log('YourName');
+
+// There are Mainly Three Types of Loops.
+/* for loop/statement
+while statement/loop
+do...while statement/loop */
+
+// Loops and Conditions are Two Main Fundamental Concepts of Core Programming
