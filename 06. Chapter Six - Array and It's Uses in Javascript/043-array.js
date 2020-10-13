1e3bac417c63925eb6191e6282bac86e815c756e
smanwarulislam <engg.anwarbd@gmail.com>
2020-10-13 22:31:59 +0600

    Add Array in JavaScript

---
+++ 06. Chapter Six - Array and It's Uses in Javascript/043-array.js
@@ -0,0 +1,8 @@
+// Chapter Six
+// What is Array in JavaScript
+
+// Working with Arrays
+
+// ***** What is Array *****
+
+// An array is a Data Structure where we can organize multiple data under a single variable
