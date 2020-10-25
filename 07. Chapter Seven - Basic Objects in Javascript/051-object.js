eff7287e26d7d46b8c7cc555603c761eb27a5571
smanwarulislam <engg.anwarbd@gmail.com>
2020-10-25 23:59:23 +0600

    Add Objects in JavaScript

---
+++ 07. Chapter Seven - Basic Objects in Javascript/051-object.js
@@ -0,0 +1,17 @@
+// Chapter Seven
+// What is Object?
+
+// Objects in JavaScript
+
+// an Object
+
+// student 1:
+/* math: 80
+chemistry: 75
+physics: 86
+higher math: 90 */
+
+// an Array
+
+// student 1:
+// [80, 75, 86, 90]
