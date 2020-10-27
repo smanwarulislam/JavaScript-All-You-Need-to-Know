220322d169b7389a23eda1e4cbebefd420758281
smanwarulislam <engg.anwarbd@gmail.com>
2020-10-27 20:20:44 +0600

    Add setting object properties

---
+++ 07. Chapter Seven - Basic Objects in Javascript/054-setting-object-properties.js
@@ -0,0 +1,33 @@
+// Chapter Seven
+// Setting Object Properties
+
+// Dot Notation
+
+var point = {
+  x: 10,
+  y: 20,
+  z: 15
+};
+
+point.x = 45;
+console.log(point);
+// expected output: {x: 45, y: 20, z: 15}
+
+point.a = 16;
+console.log(point);
+// expected output: {x: 45, y: 20, z: 15, a: 16}
+
+console.log(point.d);
+// expected output: undefined
+
+// Bracket Notation/Array Notation
+
+point['y'] = 100;
+console.log(point);
+// expected output: {x: 45, y: 100, z: 15, a: 16}
+
+var prop = 'z';
+point[prop] = 55;
+
+console.log(point);
+// expected output: {x: 45, y: 100, z: 55, a: 16}
