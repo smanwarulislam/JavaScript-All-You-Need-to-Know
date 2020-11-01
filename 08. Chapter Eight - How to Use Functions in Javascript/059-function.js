0b12883d5f384b1f6ab87615667f66cfbc07fc13
smanwarulislam <engg.anwarbd@gmail.com>
2020-11-01 23:40:18 +0600

    Add Function in JavaScript

---
+++ 08. Chapter Eight - How to Use Functions in Javascript/059-function.js
@@ -0,0 +1,16 @@
+// Chapter Eight
+// What is Function in JavaScript?
+
+// What is Function
+
+// Input Output Processing
+
+var date = new Date();
+date.getFullYear();
+
+console.log(date);
+// using the Cmder console emulator
+// expected output: 2020-11-01T17:30:39.293Z
+
+// using the Chrome DevTools Console
+// expected output: Sun Nov 01 2020 23:31:13 GMT+0600 (Bangladesh Standard Time)
