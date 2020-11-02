2b634e54a5b4e2f5a2a816a4d05a88ecb76e6b15
smanwarulislam <engg.anwarbd@gmail.com>
2020-11-02 20:20:18 +0600

    Add defining a function

---
+++ 08. Chapter Eight - How to Use Functions in Javascript/060-define-function.js
@@ -0,0 +1,18 @@
+// Chapter Eight
+// Defining a Function
+
+function functionName() {
+  // statements/code to be executed
+}
+
+function add() {
+  var a = 10;
+  var b = 20;
+  console.log(a + b);
+}
+
+function sub() {
+  var a = 50;
+  var b = 20;
+  console.log(a - b);
+}
