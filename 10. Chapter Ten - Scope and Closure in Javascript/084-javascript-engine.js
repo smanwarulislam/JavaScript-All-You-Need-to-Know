4a1e16ed1a89d11297c548e1e55bceff8015d965
smanwarulislam <engg.anwarbd@gmail.com>
2020-11-20 19:45:09 +0600

    Add how does JavaScript engine work

---
+++ 10. Chapter Ten - Scope and Closure in Javascript/084-javascript-engine.js
@@ -0,0 +1,20 @@
+// Chapter Ten
+// JavaScript Engine Inside
+
+// Scope and Closure in JavaScript
+
+// JavaScript Engine
+
+/* JavaScript Engine:
+Is another program which can read, understand and translate JavaScript code */
+
+// JavaScript Engines:
+/* Google Chrome - V8
+Mozilla Firefox - Spider Monkey
+Internet Explorer/Microsoft Edge - Chakra
+Safari - JavaScript Core */
+
+// How does JavaScript engine work?
+// Source Code -> Parser -> Abstract Window Tree -> Machine Code -> Execution
+
+// JavaScript: Compiled vs Interpreted?
