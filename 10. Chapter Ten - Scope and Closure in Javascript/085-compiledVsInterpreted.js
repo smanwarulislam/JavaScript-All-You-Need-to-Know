916f60f51d2407fc36081574367a0cd01cae1bce
smanwarulislam <engg.anwarbd@gmail.com>
2020-11-21 20:30:12 +0600

    Add JavaScript compiled vs interpreted

---
+++ 10. Chapter Ten - Scope and Closure in Javascript/085-compiledVsInterpreted.js
@@ -0,0 +1,23 @@
+// Chapter Ten
+// JavaScript Compiled or Interpreted
+
+// JavaScript Compiled vs Interpreted
+
+function a() {
+  console.log('I am a function a');
+}
+
+function b() {
+  console.log('I am a function b');
+}
+
+a();
+// expected output: I am a function a
+
+b();
+// expected output: I am a function b
+
+// Compiler
+// JIT - Just in Time
+
+// The JavaScript engine is actually a compiler
