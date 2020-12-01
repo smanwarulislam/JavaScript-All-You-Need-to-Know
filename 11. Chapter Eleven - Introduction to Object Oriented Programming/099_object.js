923d7531187dc22646f465b66ee0c050a8ff00fa
smanwarulislam <engg.anwarbd@gmail.com>
2020-12-01 23:15:22 +0600

    Add thinking everything as an object

---
+++ 11. Chapter Eleven - Introduction to Object Oriented Programming/099_object.js
@@ -0,0 +1,31 @@
+// Chapter Eleven
+// How to Think Everything as an Object
+
+// Everything which takes multiple properties to describe is an object.
+
+// object
+
+// Example
+// Person:
+/* Name
+Status
+Education
+Body
+Address */
+
+// Example
+// Book:
+/* Name
+Author
+Publication
+Total Page
+Price */
+
+// Even a number can be an object.
+// 450
+// 450.0000
+/* Integer part
+Floating part
+Negative
+Positive
+String value */
