5f38fcccf5a439fa931aa83ed222a52f12a175dc
smanwarulislam <engg.anwarbd@gmail.com>
2020-12-03 19:30:10 +0600

    Add four pillars of object-oriented programming

---
+++ 11. Chapter Eleven - Introduction to Object Oriented Programming/102_four_pillars_oop.js
@@ -0,0 +1,10 @@
+// Chapter Eleven
+// Four Pillars of Object-Oriented Programming
+
+// Object-oriented is just a theory.
+
+// OOP stands on four main pillars:
+/* Abstraction
+Encapsulation
+Inheritance
+Polymorphism */
