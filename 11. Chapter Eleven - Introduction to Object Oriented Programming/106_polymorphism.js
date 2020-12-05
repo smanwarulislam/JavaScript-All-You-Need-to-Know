12d7c2538861a63f0386aea061d4920410cb9d3f
smanwarulislam <engg.anwarbd@gmail.com>
2020-12-05 20:50:09 +0600

    Add polymorphism

---
+++ 11. Chapter Eleven - Introduction to Object Oriented Programming/106_polymorphism.js
@@ -0,0 +1,20 @@
+// Chapter Eleven
+// Take a Look at Polymorphism in OOP
+
+// What is Polymorphism?
+/* The word polymorphism is used in various contexts and describes situations in which something occurs in several different forms. In computer science, it describes the concept that objects of different types can be accessed through the same interface. Each type can provide its own, independent implementation of this interface. It is one of the core concepts of object-oriented programming (OOP). */
+
+// class
+// class (Rect)
+/* width // Properties
+height // Properties
+
+draw() // Methods */
+
+// class (Circle)
+/* radius // Properties
+
+draw() // Methods */
+
+// class (Shape) (inherit)
+// draw()
