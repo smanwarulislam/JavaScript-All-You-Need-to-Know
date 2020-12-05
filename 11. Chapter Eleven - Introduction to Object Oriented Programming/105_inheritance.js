058bc4ad6d3332dd960e340467f5d2c68ef0215c
smanwarulislam <engg.anwarbd@gmail.com>
2020-12-05 16:45:10 +0600

    Add inheritance

---
+++ 11. Chapter Eleven - Introduction to Object Oriented Programming/105_inheritance.js
@@ -0,0 +1,20 @@
+// Chapter Eleven
+// Take a Look at Inheritance in OOP
+
+// What is Inheritance?
+/* Inheritance is one of the core concepts of object-oriented programming (OOP) languages. It is a mechanism where you can derive a class from another class for a hierarchy of classes that share a set of attributes and methods. */
+
+// class
+// class (Rect)
+/* width // Properties
+height // Properties
+
+draw() // Methods */
+
+// class (Circle)
+/* radius // Properties
+
+draw() // Methods */
+
+// class (Shape) (inherit)
+// draw()
