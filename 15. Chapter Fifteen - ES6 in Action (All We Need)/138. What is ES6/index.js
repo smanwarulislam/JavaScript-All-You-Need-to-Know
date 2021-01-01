0bfaf712c46187491e715ddda2ddfadb656ea5a5
smanwarulislam <engg.anwarbd@gmail.com>
2021-01-01 21:01:57 +0600

    Add what is ES6

---
+++ 15. Chapter Fifteen - ES6 in Action (All We Need)/138. What is ES6/index.js
@@ -0,0 +1,11 @@
+// Chapter Fifteen
+// What is ES6
+
+/* The 6th edition, initially known as ECMAScript 6 (ES6) then and later renamed to ECMAScript 2015, was finalized in June 2015, was the second major revision to JavaScript. */
+
+// What does es6 stand for?
+/* ECMAScript 6
+ES6 stands for ECMAScript 6. ECMAScript was created to standardize JavaScript, and ES6 is the 6th version of ECMAScript, it was published in 2015, and is also known as ECMAScript 2015. */
+
+// What is the difference between JavaScript and es6?
+/* The ES scripting language has many implementations, and the popular one is JavaScript. Generally, ECMAScript is used for client-side scripting of the World Wide Web. ES5 is an abbreviation of ECMAScript 5 and also known as ECMAScript 2009. The sixth edition of the ECMAScript standard is ES6 or ECMAScript 6. ES6 is a significant update to the language, and the first update to the language since ES5 was standardized in 2009. */
