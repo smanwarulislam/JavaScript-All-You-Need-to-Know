93697335ab2ac7846513832d8ab5cf0e9f338b6b
smanwarulislam <engg.anwarbd@gmail.com>
2021-01-02 22:00:11 +0600

    Add setup your environment for ES6

---
+++ 15. Chapter Fifteen - ES6 in Action (All We Need)/139_Setup_Your_Environment_for_ES6/src/scripts/index.js
@@ -0,0 +1,13 @@
+import '../styles/index.scss'
+
+import { sum, sub  } from './math'
+
+console.log('Hello Webpack 4')
+
+console.log(sum(10, 20))
+console.log(sub(10, 20))
+
+// Chapter Fifteen
+// 139. Setup Your Environment for ES6
+
+alert('Yes, I have done everything perfectly.')
