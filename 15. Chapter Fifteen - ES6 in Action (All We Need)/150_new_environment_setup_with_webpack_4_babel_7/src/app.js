3943d46c9351d4fc79e7b2a7b98e47673f5eb514
smanwarulislam <engg.anwarbd@gmail.com>
2021-01-17 19:20:59 +0600

    Add Create Iterator with ES6 Symbol.Iterator Function

--- 15. Chapter Fifteen - ES6 in Action (All We Need)/150_new_environment_setup_with_webpack_4_babel_7/src/app.js
+++ 15. Chapter Fifteen - ES6 in Action (All We Need)/150_new_environment_setup_with_webpack_4_babel_7/src/app.js
@@ -11,7 +11,7 @@ const arr = [1, 2, 3];
   console.log(arr[i]);
 } */
 
-function createIterator(collection) {
+/* function createIterator(collection) { // Interface/function
   let i = 0;
   return {
     next() { // Method
@@ -21,21 +21,21 @@ function createIterator(collection) {
       };
     }
   };
-}
+} */
 
-let iterate = createIterator(arr);
-console.log(iterate);
+// let iterate = createIterator(arr);
+// console.log(iterate);
 // expected output: (See the Chrome DevTools Console)
 /* -> {next: ƒ}
   -> next: ƒ next() */
 
-console.log(iterate.next());
+// console.log(iterate.next());
 // expected output: (See the Chrome DevTools Console)
 /* -> {done: false, value: 1}
   done: false
   value: 1 */
 
-console.log(iterate.next());
+// console.log(iterate.next());
 // expected output: (See the Chrome DevTools Console)
 /* -> {done: false, value: 2}
   done: false
@@ -49,14 +49,86 @@ console.log(iterate.next());
       -> collection: (3) [1, 2, 3]
         i: 2 */
 
-console.log(iterate.next());
+// console.log(iterate.next());
 // expected output: (See the Chrome DevTools Console)
 /* -> {done: false, value: 3}
   done: false
   value: 3 */
 
-console.log(iterate.next());
+// console.log(iterate.next());
 // expected output: (See the Chrome DevTools Console)  
 /* -> {done: true, value: undefined}
+  done: true
+  value: undefined */
+
+// Chapter Fifteen  
+// 152. Create Iterator with ES6 Symbol.Iterator Function
+
+// console.log(arr[Symbol.iterator]); // The Symbol is a unique identifier
+// expected output: (See the Chrome DevTools Console)
+// ƒ values() { [native code] }
+
+// console.log('str'[Symbol.iterator]);
+// expected output: (See the Chrome DevTools Console)
+// ƒ [Symbol.iterator]() { [native code] }
+
+// In the case of arrays
+let iterate = arr[Symbol.iterator](); // Symbol.iterator -> Already the interface is in an implementation state
+
+console.log(iterate.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: 1, done: false}
+  done: false
+  value: 1 */
+
+console.log(iterate.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: 2, done: false}
+  done: false
+  value: 2 */
+
+console.log(iterate.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: 3, done: false}
+  done: false
+  value: 3 */
+
+console.log(iterate.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> value: undefined, done: true}
+  done: true
+  value: undefined */
+
+// In the case of string
+let str = 'TEXT';
+let iterateText = str[Symbol.iterator]();
+
+console.log(iterateText.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: "T", done: false}
+  done: false
+  value: "T" */
+
+console.log(iterateText.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: "E", done: false}
+  done: false
+  value: "E" */
+
+console.log(iterateText.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: "X", done: false}
+  done: false
+  value: "X" */
+
+console.log(iterateText.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: "T", done: false}
+  done: false
+  value: "T" */
+
+console.log(iterateText.next());
+// expected output: (See the Chrome DevTools Console)
+/* -> {value: undefined, done: true}
   done: true
   value: undefined */
