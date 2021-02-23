8ca4daae3cc906262010c9747e7236d355f718c4
smanwarulislam <engg.anwarbd@gmail.com>
2021-02-24 01:10:12 +0600

    Add Introduction to DOM - Document Object Model

---
+++ 18-chapter-eighteen-dom-(document-object-model)-manipulation-with-ajax/src/lessons/lesson1.js
@@ -0,0 +1,16 @@
+// Chapter Eighteen
+// 195. Introduction to DOM - Document Object Model
+
+// What is DOM (Document Object Model)?
+// It just an API to interact with HTML document and change it later on demand.
+
+// DOM is a web API, a web API that is provided to us by the browser and has nothing to do with the JavaScript engine.
+
+// In fact, a DOM is actually a data structure (Tree data structure).
+
+// In the Document Object Model, we have to work with many nodes
+// There are the main 4 nodes
+/* - Element Node
+- Text Node
+- Attribute Node
+- Comment Node */
